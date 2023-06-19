2809c34ad40fbfa7d7e5ce9dca024b8ee1697367
oleetech <olee.techs@gmail.com>
2023-06-19 23:40:46 +0600

    salesorder total price calculation and show total amount

--- static/js/salesorder.js
+++ static/js/salesorder.js
@@ -1,77 +1,50 @@
-(function ($) {
-    $(document).ready(function(){
+(function($) {
+  $(document).ready(function() {
+    // Your code here
 
+    // Example: Changing IDs with prefix
+    var prefix = '#id_salesorderitem_set-';
+    var pricePrefix = '#id_salesorderitem_set-{0}-Price';
+    var quantityPrefix = '#id_salesorderitem_set-{0}-Quantity';
+    var priceTotalPrefix = '#id_salesorderitem_set-{0}-PriceTotal';
 
-// Function to calculate the sum of Quantity * Price and set the value in PriceTotal fields
-function calculateTotal() {
-  var rows = document.querySelectorAll("tbody tr.has_original"); // Select all rows with class 'has_original'
-  var total = 0;
-
-  rows.forEach(function(row) {
-    var quantity = parseInt(row.querySelector(".field-Quantity input").value);
-    var price = parseFloat(row.querySelector(".field-Price input").value);
-    var priceTotalField = row.querySelector(".field-PriceTotal input");
-    
-    if (!isNaN(quantity) && !isNaN(price)) {
-      var priceTotal = quantity * price;
-      total += priceTotal;
-      priceTotalField.value = priceTotal.toFixed(4);
+    // Example: Calculate PriceTotal
+    function calculatePriceTotal(index) {
+      var price = parseFloat($(pricePrefix.replace('{0}', index)).val());
+      var quantity = parseFloat($(quantityPrefix.replace('{0}', index)).val());
+      var priceTotal = price * quantity;
+      $(priceTotalPrefix.replace('{0}', index)).val(priceTotal.toFixed(4));
     }
+
+    // Example: Calculate the sum of all calculatePriceTotal values
+    function calculateTotalAmount() {
+      var total = 0;
+      $('input[name^="salesorderitem_set-"][name$="-PriceTotal"]').each(function() {
+        var priceTotal = parseFloat($(this).val());
+        if (!isNaN(priceTotal)) {
+          total += priceTotal;
+        }
+      });
+      return total.toFixed(4);
+    }
+
+    // Example: Set the TotalAmount value
+    function setTotalAmount() {
+      var totalAmount = calculateTotalAmount();
+      $('#id_TotalAmount').val(totalAmount);
+    }
+
+    // Example: Handle changes in Price and Quantity fields
+    function handleFieldChanges() {
+      $('input[name^="salesorderitem_set-"][name$="-Price"], input[name^="salesorderitem_set-"][name$="-Quantity"]').on('change', function() {
+        var index = $(this).attr('name').split('-')[1];
+        calculatePriceTotal(index);
+        setTotalAmount();
+      });
+    }
+
+    // Call the function to handle field changes
+    handleFieldChanges();
+
   });
-
-  // Set the calculated total in the input field
-  document.getElementById("id_TotalAmount").value = total.toFixed(4);
-}
-
-// Add event listeners to Quantity and Price input fields
-var quantityInputs = document.querySelectorAll(".field-Quantity input");
-var priceInputs = document.querySelectorAll(".field-Price input");
-
-quantityInputs.forEach(function(input) {
-  input.addEventListener("input", function() {
-    calculateTotal();
-    var row = input.closest("tr");
-    var priceTotalField = row.querySelector(".field-PriceTotal input");
-    priceTotalField.value = (parseInt(input.value) * parseFloat(row.querySelector(".field-Price input").value)).toFixed(4);
-  });
-});
-
-priceInputs.forEach(function(input) {
-  input.addEventListener("input", function() {
-    calculateTotal();
-    var row = input.closest("tr");
-    var priceTotalField = row.querySelector(".field-PriceTotal input");
-    priceTotalField.value = (parseInt(row.querySelector(".field-Quantity input").value) * parseFloat(input.value)).toFixed(4);
-  });
-});
-
-
-
-
-
-        $('#id_CustomerName, #id_Address').on('change',function(){
-            var initialname = $('#id_CustomerName').val();
-            var initialquantity = $('#id_Address').val();
-            if (initialname !== '' && initialquantity !== '') {
-                $.ajax({
-                    type: 'POST',
-                    url: '/library/ajax/',
-                    data: {
-                        'name': initialname,
-                        'quantity': initialquantity
-                    },
-                    dataType: 'json',
-                    success: function(response){
-                        // updateFormset(response);
-                        // $('#id_production_components-0-quantity').val(response.quantity);
-
-                    }
-                });
-            }
-
-        });
-
-
-
-    });
 })(jQuery);
